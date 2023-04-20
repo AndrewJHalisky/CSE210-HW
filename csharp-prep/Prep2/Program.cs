f853e322df64af02a12451934e2f2e45391e4f68
AndrewJHalisky <ajhalisky@gmail.com>
2023-04-20 17:29:41 -0400

    Finshed prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your grade number: ");
+        string answer = Console.ReadLine();
+        int number = int.Parse(answer);
+
+        string grade = "";
+        
+        if (number >= 90)
+        {
+            grade = "A";
+        }
+        else if (number >= 80)
+        {
+            grade = "B";
+        }
+        else if (number >= 70)
+        {
+            grade = "C";
+        }
+        else if (number >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+        Console.WriteLine($"Your grade is a {grade}");
+        
+        if (number >= 70)
+        {
+            Console.WriteLine("Congradulations! You passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry you did not pass the course. Please retake it again.");
+        }
     }
 }
