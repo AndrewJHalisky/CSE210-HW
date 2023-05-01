98650d55e01010e9f12736bcc587f88769703564
AndrewJHalisky <ajhalisky@gmail.com>
2023-05-01 19:03:10 -0400

    Finsihed learning 02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Felix inc.";
+        job1._startYear = 2025;
+        job1._endYear = 2065;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Troubleshooter";
+        job2._company = "Wix";
+        job2._startYear = 2027;
+        job2._endYear = 2048;
+
+        Resume myResume = new Resume();
+        myResume._name = "Andrew Halisky";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+
     }
 }
